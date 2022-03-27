34ca522901ff7e06a2ff6ed754ee684bf3a49019
Mshary <m.alsharekh94@gmail.com>
2022-03-27 22:53:10 +0300

    Display full horse stats

--- src/components/Horses/Horse.js
+++ src/components/Horses/Horse.js
@@ -3,8 +3,14 @@ import React from "react";
 const Horse = ({ horse }) => {
   return (
     <div>
-      <p>{horse.name}</p>
       <img src={horse.image} alt={horse.name} />
+      <p>{horse.name}</p>
+      <p>Attack: {horse.attack}</p>
+      <p>Defence: {horse.defence}</p>
+      <p>Stamina: {horse.stamina}</p>
+      <p>Level Required: {horse.level}</p>
+      <p>Price: {horse.price} Gold</p>
+      {horse.premium && <p>-Premium-</p>}
     </div>
   );
 };
