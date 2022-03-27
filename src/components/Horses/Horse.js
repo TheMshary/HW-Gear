00e402fc821f3ba52a442b03f17ad24e3cb3333e
Mshary <m.alsharekh94@gmail.com>
2022-03-27 23:24:17 +0300

    Added size scaler to the Horse item UI

--- src/components/Horses/Horse.js
+++ src/components/Horses/Horse.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Horse = ({ horse }) => {
+  // itemWidth is the size multiplier on the whole Horse item UI
+  const itemWidth = 0.7;
   return (
-    <div style={{ width: "316px" }}>
-      <img src={horse.image} alt={horse.name} />
+    <div style={{ width: `${316 * itemWidth}px` }}>
+      <img src={horse.image} width={`${316 * itemWidth}px`} alt={horse.name} />
       <p style={{ textAlign: "center" }}>{horse.name}</p>
       <p>Attack: {horse.attack}</p>
       <p>Defence: {horse.defence}</p>
