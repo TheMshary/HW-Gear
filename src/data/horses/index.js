02dbc43f5d27aa7d92210f857695d229e52a5b49
Mshary <m.alsharekh94@gmail.com>
2022-03-27 23:37:00 +0300

    Added 6 more horses

--- src/data/horses/index.js
+++ src/data/horses/index.js
@@ -4,6 +4,12 @@ const welsh = require("./images/welsh.jpeg");
 const fjordHorse = require("./images/fjord-horse.jpeg");
 const holsteiner = require("./images/holsteiner.jpeg");
 const rottaler = require("./images/rottaler.jpeg");
+const eastFrisian = require("./images/east-frisian.jpeg");
+const babaryHorse = require("./images/babary-horse.jpeg");
+const einsiedler = require("./images/einsiedler.jpeg");
+const arabHorse = require("./images/arab-horse.jpeg");
+const brabantHorse = require("./images/brabant-horse.jpeg");
+const alpineHorse = require("./images/alpine-horse.jpeg");
 
 const data = [
   {
@@ -64,6 +70,66 @@ const data = [
     level: 15,
     price: 5000,
     image: rottaler,
+    premium: false,
+  },
+  {
+    name: "East Frisian",
+    defence: 6,
+    attack: 9,
+    stamina: 9,
+    level: 17,
+    price: 10000,
+    image: eastFrisian,
+    premium: false,
+  },
+  {
+    name: "Babary Horse",
+    defence: 8,
+    attack: 13,
+    stamina: 10,
+    level: 20,
+    price: 15000,
+    image: babaryHorse,
+    premium: false,
+  },
+  {
+    name: "Einsiedler",
+    defence: 9,
+    attack: 13,
+    stamina: 13,
+    level: 20,
+    price: 15000,
+    image: einsiedler,
+    premium: false,
+  },
+  {
+    name: "Arab Horse",
+    defence: 10,
+    attack: 15,
+    stamina: 15,
+    level: 23,
+    price: 20000,
+    image: arabHorse,
+    premium: false,
+  },
+  {
+    name: "Brabant Horse",
+    defence: 12,
+    attack: 17,
+    stamina: 17,
+    level: 25,
+    price: 25000,
+    image: brabantHorse,
+    premium: false,
+  },
+  {
+    name: "Alpine Horse",
+    defence: 15,
+    attack: 20,
+    stamina: 20,
+    level: 30,
+    price: 30000,
+    image: alpineHorse,
     premium: false,
   },
 ];
