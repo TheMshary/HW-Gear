41aec1b4a333d708685cacf6534e8fdc76282a92
Mshary <m.alsharekh94@gmail.com>
2022-04-07 06:21:19 +0300

    Added comments to clarify what my next step is.

--- src/components/Horses/index.js
+++ src/components/Horses/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import horses from "../../data/horses";
 import Horse from "./Horse";
 
+// TODO: Carousel
+// Create carousel manually for customized behavior
+// To make it work for PCs easily, allow it to scroll
+//  right when user scrolls down or right, and scroll
+//  left when user scrolls up or left.
+// Guide: https://bit.ly/3NXlriC
+
 const Horses = () => {
   const horseList = horses.map((horse) => <Horse horse={horse} />);
   return <div style={{ display: "flex" }}>{horseList}</div>;
